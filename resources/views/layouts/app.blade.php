82fcfc851760aab5583998583ba739eea157ea7a
joaocuculo <jv.cuculo@gmail.com>
2026-08-21 13:28:26 -0300

    chore: setup Laravel development environment

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'Memórias Vivas' }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+
+<body>
+
+    {{ $slot }}
+
+    @livewireScripts
+</body>
+
+</html>
